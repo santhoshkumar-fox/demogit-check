51b618edeb8c2cfb12b6d5bf3e598eea5c32aa08
santhoshvellingiri2022 <106522483+santhoshvellingiri2022@users.noreply.github.com>
2022-09-07 12:48:25 +0530

    todo listercomplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,60 @@
-import React from 'react'
-import Home from './components/Home/Home'
-import Popup from './components/popup/Popup'
-import "./app.css"
+import React from "react";
+import Home from "./components/Home/Home";
+import Popup from "./components/popup/Popup";
+import "./app.css";
 
 const App = () => {
   return (
-    <div className='containerApp'>
-        <Home/>
-        <Popups  data={["santhosh","vellingiri"]}/>
-        
-     
+    <div className="containerApp">
+      <Home />
+      <Popups data={["santhosh", "vellingiri"]} />
     </div>
-  )
-}
-const showallData =()=>{
+  );
+};
 
+// ############################# data_init ######################################
+
+const showallData = () => {
   // let [alldata ,setAllData] = useState({})
 
   const localLength = localStorage.length;
-  console.log("[Storage-Length]",localLength);
+  console.log("[Storage-Length]", localLength);
   const all_datum = [];
 
-  for (var i=0;i <= localLength;i++){
-    
-    const getName = localStorage.key(i)
+  for (var i = 0; i <= localLength; i++) {
+    const getName = localStorage.key(i);
 
-    if(getName !== null){
-    const spliter = getName.split('h');
-    console.log("[spliter]")
-    console.log(spliter[0]);
-    const first_value = spliter[0];
-    if(first_value === "sant"){
-      try {
-        const obj_data = JSON.parse(localStorage.getItem(getName))
-      console.log(obj_data);
-     all_datum.push(obj_data)
-      } catch (error) {
-        console.error(error)
+    if (getName !== null) {
+      const spliter = getName.split("h");
+      console.log("[spliter]");
+      console.log(spliter[0]);
+      const first_value = spliter[0];
+      if (first_value === "sant") {
+        try {
+          const obj_data = JSON.parse(localStorage.getItem(getName));
+          console.log(obj_data);
+          all_datum.push(obj_data);
+        } catch (error) {
+          console.error(error);
+        }
       }
     }
-    }
-    
-    
-
   }
-  console.log("[ALL]    ",all_datum);
+  console.log("[ALL]    ", all_datum);
 
   return all_datum;
-  
-}
+};
 
-const Popups = (props)=>{
-  console.log("HI")
+// ############################# data_pop_Container_component ######################################
+
+const Popups = (props) => {
+  console.log("HI");
   let data1 = showallData();
-  const f = data1.map((e,index)=>{
-    console.log('hi');
-    return <Popup name={e.data} key={index} id={e.id}/>
-  })
+  const f = data1.map((e, index) => {
+    console.log("hi");
+    return <Popup name={e.data} key={index} id={e.id} />;
+  });
   return f;
-}
+};
 
-export default App
+export default App;
--- src/components/popup/Popup.jsx
+++ src/components/popup/Popup.jsx
@@ -1,55 +1,80 @@
-import React, { useEffect, useState } from 'react'
-import "./popup.css"
-import {AiFillEdit,AiFillDelete} from"react-icons/ai"
-import PopUPWin from '../PopUPWin/PopUPWin'
+import React, { useEffect, useState } from "react";
+import "./popup.css";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import PopUPWin from "../PopUPWin/PopUPWin";
+
+
+// ############################# Popup_main_component ######################################
+
 const Popup = (props) => {
+  const [triger, setTrigger] = useState(true);
+  const [valueedit, setValue] = useState("");
 
-
-  const [triger,setTrigger] = useState(true);
-  const [valueedit ,setValue] = useState('');
-
-  window.addEventListener('scroll',(e)=>{
+  window.addEventListener("scroll", (e) => {
     console.log(e);
-  })
+  });
+// ############################# deleter ######################################
 
-  const Deleter = (value)=>{
-
+  const Deleter = (value) => {
     localStorage.removeItem(`${value}`);
-    console.log("[Deleted😶‍🌫️]")
+    console.log("[Deleted😶‍🌫️]");
     window.location.reload();
-   
-    
-  }
+  };
+// ############################# Editor ######################################
 
-  const Editer = (value)=>{
+  const Editer = (value) => {
     setTrigger(false);
-    
-  }
-  const btn_edit = ()=>{
+  };
 
-    console.log("[id]       ",props.id)
-    console.log("[value]    ",valueedit)
-    const data_obj = {data:String(valueedit),ischecked:false,id:`${props.id}`};
-    localStorage.setItem(`${props.id}`,JSON.stringify(data_obj));
+// ############################# button_Updater in localstrorage ######################################
+
+  const btn_edit = () => {
+    console.log("[id]       ", props.id);
+    console.log("[value]    ", valueedit);
+    const data_obj = {
+      data: String(valueedit),
+      ischecked: false,
+      id: `${props.id}`,
+    };
+    localStorage.setItem(`${props.id}`, JSON.stringify(data_obj));
     setTrigger(true);
     window.location.reload();
-  }
-  const changer_edit = (e)=>{
+  };
+
+// ############################# btn_state_updater ######################################
+
+  const changer_edit = (e) => {
     setValue(e.target.value);
-  }
+  };
 
   return (
-    <div className='containerPop' id='containerPop'>
-      {triger?<p>{props.name}</p>:<div><input className='editer' placeholder={props.name} onChange={changer_edit}></input><button id='btn'  onClick={btn_edit}>Edit</button></div>}
-      <div className='icon_con'>
-        <a onClick={()=>Editer(props.id)}><AiFillEdit/></a>
-        <a onClick={()=>Deleter(props.id)}><AiFillDelete/></a>
+    <div className="containerPop" id="containerPop">
+     
+     {/* Edit trigger */}
+
+      {triger ? (
+        <p>{props.name}</p>) : (
+          <div>
+          <input
+            className="editer"
+            placeholder={props.name}
+            onChange={changer_edit}
+          ></input>
+          <button id="btn" onClick={btn_edit}>
+            Edit
+          </button>
+        </div>
+      )}
+      <div className="icon_con">
+        <a onClick={() => Editer(props.id)}>
+          <AiFillEdit />
+        </a>
+        <a onClick={() => Deleter(props.id)}>
+          <AiFillDelete />
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-
-
-export default Popup
+export default Popup;
